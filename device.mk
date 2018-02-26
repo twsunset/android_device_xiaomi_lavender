3fc7464b9242469dc1f46d7efb6e4a9b465fd796
Shubham Das <dasshubham762@gmail.com>
2018-02-26 18:37:54 +0530

    add miui power off charging animation

--- device.mk
+++ device.mk
@@ -167,6 +167,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Charger
+include $(LOCAL_PATH)/rootdir/charger/charger.mk
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
